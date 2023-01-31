15bf86e3524599435ef5b9f36ba2665aeb0ee658
3bada-191 <85360948+3bada-191@users.noreply.github.com>
2023-02-01 00:43:00 +0300

    Add files via upload

--- cart.php
+++ cart.php
@@ -58,13 +58,12 @@
         
     <?php 
         global $connect;
-    $ip=getIp();
-
-    if (isset($_POST['update_cart'])) {
+        $ip=getIp();
+    if (isset($_POST['update_car'])) {
       foreach($_POST['obd'] as $key => $rev) {
-       $update_rev = "update cart set raw='$rev' where p_id = '$key' AND ip_add = '$ip'";
+       $update_rev = "update cart set raw='$rev' where p_id = '$pro_id_t' AND ip_add = '$ip'";
        $run_u_rev = mysqli_query($connect, $update_rev);
-       $_SESSION['obd'][] = $rev;
+       $_SESSION['obd'] = $rev;
         
     } }?>
         
